1c53db57990b5c79466f2cffd9d7e338407079d7
galamo <galamouyal88@gmail.com>
2023-08-23 20:59:17 +0300

    a

---
+++ src/app/album/album.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-album',
+    templateUrl: './album.component.html',
+    styleUrls: ['./album.component.css']
+})
+export class AlbumComponent {
+
+    constructor() { }
+}
